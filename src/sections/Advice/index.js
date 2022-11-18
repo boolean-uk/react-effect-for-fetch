82a3ffde7327eabe62f3d899aa1c77b0a8894013
Fatima <fxtimaahmed14@gmail.com>
2022-11-18 12:36:10 +0000

    framework for advice section set out

--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,13 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import AdviceSlip from "./components/AdviceSlip";
+import FavoriteSlipList from "./components/FavouriteSlipsList";
 
 function AdviceSection() {
+  const [advice, setAdvice] = useState("");
+  const [favouriteAdvice, setFavourtitAdvice] = useState([]);
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section class="adivce-slip">
+        <h3>Some Advice</h3>
+        <AdviceSlip />
+        <button>Get More Advice</button>
+        <button>Save to Favourties</button>
+      </section>
+      <section class="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          <FavoriteSlipList />
+        </ul>
+      </section>
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
