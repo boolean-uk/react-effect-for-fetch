b84e59052edd368b6d77ce7563cfd43e2f208953
Fatima <fxtimaahmed14@gmail.com>
2022-11-18 13:04:35 +0000

    lots of Ritthy Ryan's rendering !

---
+++ src/sections/Users/components/UsersListItem.js
@@ -0,0 +1,14 @@
+function UsersListItem() {
+  return (
+    <li class="bg-blue">
+      <img
+        src="https://randomuser.me/api/portraits/med/men/76.jpg"
+        alt="Ritthy Ryan"
+      />
+      <h3>Mr Ritthy Ryan</h3>
+      <p>Email: ritthy.ryan@example.com</p>
+    </li>
+  );
+}
+
+export default UsersListItem;
