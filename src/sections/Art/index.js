9877f4d36edb896de79802320f837e4fc5f11737
Dave <Daverevell491@gmail.com>
2023-02-14 17:27:26 +0000

    Art section created in index

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,12 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function ArtsSection() {
-  return (
-    <section>
-      <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
+	const [artList, setArtList] = useState([]);
+
+	useEffect(() => {
+		fetch("https://api.artic.edu/api/v1/artworks")
+			.then((res) => res.json())
+			.then((data) => setArtList(data.data));
+	}, []);
+
+	return (
+		<section>
+			<h2>Arts Section</h2>
+			<div className="scroll-container">
+				<ul className="art-list">
+					{artList.map((item) => {
+						const { title, artist_title, subject_titles, image_id } = item;
+						const image = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`;
+						return (
+							<li>
+								<img src={image} alt={title} />
+								<h3>{title}</h3>
+								<p> Artist: {artist_title}</p>
+								<h4>Subjects:</h4>
+								<ul>
+									{subject_titles.map((subject) => {
+										return <li>{subject}</li>;
+									})}
+								</ul>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</section>
+	);
 }
 
-export default ArtsSection
+export default ArtsSection;
