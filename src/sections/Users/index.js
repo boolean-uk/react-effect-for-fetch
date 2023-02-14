d1e58d469ffe4176968b77309f38e53a8dbb10a5
Dave <Daverevell491@gmail.com>
2023-02-14 18:06:02 +0000

    Users section created in index

--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,12 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function UsersSection() {
-  return (
-    <section>
-      <h2>Users Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetch("https://randomuser.me/api/?results=5")
+			.then((res) => res.json())
+			.then((data) => setUsers(data.results));
+	}, []);
+
+	return (
+		<section>
+			<h2>Users Section</h2>
+			<div className="scroll-container">
+				<ul className="users-list">
+					{users.map((user) => {
+						const { name, email, picture } = user;
+						return (
+							<li class={user.gender === "male" ? "bg-red" : "bg-blue"}>
+								<img src={picture.large} alt="User pfp" />
+								<h3>{`${name.title} ${name.first} ${name.last}`}</h3>
+								<p>Email: {email}</p>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</section>
+	);
 }
 
-export default UsersSection
+export default UsersSection;
