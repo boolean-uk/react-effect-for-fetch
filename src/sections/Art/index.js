a169d2293699fe302e880277109ddc4d21cd33b7
Georgopoulos Giorgos <dgiorgos90@gmail.com>
2023-08-22 02:18:47 +0300

    fetching data fro api

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,12 +1,37 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function ArtsSection() {
+  const [artData, setArtData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const response = await fetch("https://api.artic.edu/api/v1/artworks");
+    const json = await response.json();
+    setArtData(json.data);
+    console.log(json.data);
+  }
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      {artData.map((artwork) => (
+        <div>
+          <h4> Title : {artwork.title}</h4>
+          <h4> Artist : {artwork.artist_title}</h4>
+          <p>
+            Subjects:
+            {artwork.subject_titles.map((title) => (
+              <p>{title}</p>
+            ))}
+          </p>
+        </div>
+      ))}
+      <div className="scroll-container">{artData.map}</div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,12 +1,34 @@
 import { useEffect, useState } from "react"
 
 function UsersSection() {
+const [usersData, setUsersData] =  useState([]);
+
+ useEffect(() => {
+ getData();
+}, []);
+
+ async function getData() {
+    const response = await fetch("https://randomuser.me/api/");
+    const json = await response.json();
+    setUsersData(json);
+    console.log(json);
+  }
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      {usersData.map((user) => (
+        <div>
+          <h4> fullName: {user.first} {user.last}</h4>
+          <h4> email : {user.email}</h4>
+          
+        </div>
+      ))}
     </section>
-  )
-}
+  );
 
+
+
+
+}
 export default UsersSection
