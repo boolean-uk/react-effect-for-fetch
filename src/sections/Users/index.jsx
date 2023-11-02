fe6d10c9327181bf11bab329e0e12179523747d2
Lukas Dembicki <lad_777@hotmail.com>
2023-11-02 17:49:04 +0000

    art and users rendering

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
+
 function UsersSection() {
+
+  const [users, setUsers] = useState([])
+
+  const getUsers = () => {
+    fetch('https://randomuser.me/api/?results=10')
+      .then(res => res.json())
+      .then(data => setUsers(data.results))
+  }
+
+  useEffect(getUsers, [])
+
+
+
+
+  const user = users.map((user, index) => (
+    <li key={index} className="bg-blue">
+      <img
+        src= {`${user.picture.large}`}
+        alt="Ritthy Ryan"
+      />
+      <h3>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h3>
+      <p>Email: {user.email}</p>
+    </li>
+  ))
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {user}
+        </ul>
+      </div>
     </section>
   )
 }
