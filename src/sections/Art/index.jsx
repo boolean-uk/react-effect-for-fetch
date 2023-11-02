d207d91d8037bb10ae7ea5729c5742e158fad007
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-11-03 00:30:57 +0100

    exercise done

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,17 @@
-function ArtsSection() {
+import React from "react";
+import ArtList from "./components/ArtList";
+
+const ArtsSection = () => {
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          <ArtList />
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ArtsSection
+export default ArtsSection;
