e0d39e04f21ea5309f827d989239fd5f9a9b8f90
TomEastwood <t.eastwood99@gmail.com>
2023-11-02 17:41:24 +0000

    succesful API call and art items rendering

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react'
+import ArtListItem from './components/ArtListItem'
+
 function ArtsSection() {
+
+  const [artData, setArtData] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.artic.edu/api/v1/artworks')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setArtData(data.data)
+        console.log(data)
+      })
+  }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">{artData.map((artItem) => {
+        return (
+          <ArtListItem 
+          title={artItem.title}
+          artist_title={artItem.artist_title}
+          />
+        )
+      })}
+      </div>
     </section>
   )
 }
 
 export default ArtsSection
+
+
