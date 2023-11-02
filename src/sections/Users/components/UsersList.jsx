cb3cd058dccef972f1b540e7f40b6344eee75d3a
Satoki <satokiito@gmail.com>
2023-11-02 17:43:55 +0000

    core (Users) - UsersListItem component extracted out

--- src/sections/Users/components/UsersList.jsx
+++ src/sections/Users/components/UsersList.jsx
@@ -1,26 +1,11 @@
+import UsersListItem from "./UsersListItem"
+
 function UsersList({ users}) {
-    
-    function changeBgColour(user) {
-        if (user.gender === 'male') return "bg-blue"
-        else return "bg-pink"
-      }
-    
-      function formatName(user) {
-        const nameGroup = user.name
-        return `${nameGroup.title} ${nameGroup.first} ${nameGroup.last}`
-      }
     
     return (
         <ul className="users-list">
           {users.map((user, index) => 
-            <li key={index} className={changeBgColour(user)}>
-            <img
-              src={user.picture.medium}
-              alt={`${user.name.first} ${user.name.last}`}
-            />
-            <h3>{formatName(user)}</h3>
-            <p>Email: {user.email}</p>
-          </li>
+            <UsersListItem key={`${user.name.last}-${index}`} user={user} />
           )}
         </ul>
     )
