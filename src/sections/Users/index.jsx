6af8b618bab52f1579ba857b5ecdeee994e32fc7
Satoki <satokiito@gmail.com>
2023-11-02 16:59:21 +0000

    core (Users) - added placeholder 1x male & 1x female user for visibility

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -2,7 +2,26 @@ function UsersSection() {
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          <li className="bg-blue">
+            <img
+              src="https://randomuser.me/api/portraits/med/men/76.jpg"
+              alt="Ritthy Ryan"
+            />
+            <h3>Mr Ritthy Ryan</h3>
+            <p>Email: ritthy.ryan@example.com</p>
+          </li>
+          <li className="bg-pink">
+            <img
+              src="https://randomuser.me/api/portraits/med/women/53.jpg"
+              alt="Chloe Lopez"
+            />
+            <h3>Ms Chloe Lopez</h3>
+            <p>Email: chloe.lopez@example.com</p>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
