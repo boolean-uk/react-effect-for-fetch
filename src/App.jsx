a2682e377d2ccc648d9bf7f3eb6b9ea6c752fc97
Faiza <faraz_ul_islam@yahoo.com>
2023-11-04 22:43:26 +0000

    core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import AdviceSection from './sections/Advice'
 import ArtsSection from './sections/Art'
 import UsersSection from './sections/Users'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
+
 function App() {
+  const [usersSection, setUsersSection] = useState([])
+  const [artSection, setArtSection] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=10')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsersSection(data.results)
+        console.log(data)
+
+      })
+  }, [])
+
+
+  // art fetch 
+  useEffect(() => {
+    fetch('https://api.artic.edu/api/v1/artworks?limit=100')
+      .then((response) => response.json())
+      .then((data) => {
+        setArtSection(data.data)
+        console.log(data.data)
+      })
+  }, [])
+
+
+
   return (
     <div className="main-layout">
-      <ArtsSection />
-      <UsersSection />
+      <ArtsSection  artSection = {artSection}/>
+      <UsersSection usersSection={usersSection} />
       <AdviceSection />
     </div>
   )
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,34 @@
-function UsersSection() {
+
+
+
+function UsersSection(props) {
+const {usersSection } = props
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+       
+    
+      <div className="scroll-container">
+        <ul>
+          {usersSection.map((user) => {
+            const className = user.gender === 'male'? 'bg-blue' : 'bg-pink'
+            const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`
+            const email = user.email ? `Email: ${user.email}` : '';
+            const phonNumber = user.phone ? `Number: ${user.phone}` : '';
+
+            return (
+              <li key={user.id.value} className={className}> 
+                <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
+                <h3>{fullName}</h3>
+                {email && <p>{email}</p>}
+                {phonNumber && <p>{phonNumber}</p>}
+
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </section>
   )
 }
