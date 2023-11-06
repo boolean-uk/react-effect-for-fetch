f81630ca28dc008081897789d74731f6e5ca7dd3
atiqur rahman <atiq.ar@gmail.com>
2023-11-06 08:10:16 +0000

    commit

--- src/sections/Users/User.jsx
+++ src/sections/Users/User.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from 'react';
 
-function UsersSection() {
-  const [usersSection, setUsersSection] = useState([]);
+useEffect(() => {
+  fetch('https://api.artic.edu/api/v1/artworks?limit=100') 
+     .then((response)=> response.json())
+     .then((data) => {
+      setArtSection(data.data)
+      console.log(data.data)
+    })
+  }, []) , /*this is the useEffect for the fetch request*/
 
-  useEffect(() => {
-    fetch('https://randomuser.me/api/?results=10')
-      .then((response) => response.json())
-      .then((data) => {
-        setUsersSection(data.results);
-        console.log(data);
-      });
-  }, []);
+function UsersSection(props) {
+
+useEffect(() => {
+      fetch('https://randomuser.me/api/?results=10') 
+        .then((response)=> response.json())
+        .then((data) =>  {setUsersSection(data.results)
+          console.log(data)
+        
+        })
+  }, []) , /*this for the user section , but this in the app.jsx*/
+
+  const { usersSection } = props
 
   return (
     <section>
@@ -18,22 +28,26 @@ function UsersSection() {
       <div className="scroll-container">
         <ul>
           {usersSection.map((user) => {
-            const className = user.gender === 'male' ? 'bg-blue' : 'bg-pink';
-            const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+            const className = user.gender === 'male'? 'bg-blue' : 'bg-pink'
+            const fullName = `${user.name.title} ${user.name.first} ${user.name.last}`
             const email = user.email ? `Email: ${user.email}` : '';
 
             return (
-              <li key={user.login.uuid} className={className}>
+              <li key={user.id.value} className={className}> 
                 <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
                 <h3>{fullName}</h3>
+                {/* <p>{email}</p> */}
                 {email && <p>{email}</p>}
+  
               </li>
-            );
+            )
           })}
         </ul>
       </div>
+      
+
     </section>
-  );
+  )
 }
 
-export default UsersSection;
+export default UsersSection
