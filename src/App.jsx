77266a8e9a666e2e2a86302c9113a206cd411157
Papi <test@Tests-Air.fritz.box>
2023-11-06 14:39:40 +0100

    Papi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,35 @@ import AdviceSection from './sections/Advice'
 import ArtsSection from './sections/Art'
 import UsersSection from './sections/Users'
 import './App.css'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  const [userSection, setUserSection] = useState([])
+  const [artSection, setArtSection] = useState([])
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=10') 
+      .then((response)=> response.json())
+      .then((data) =>  {
+        setUserSection(data.results)
+        console.log(data.results)
+
+      })
+}, [])
+
+useEffect(() => {
+  fetch('https://api.artic.edu/api/v1/artworks?limit=100') 
+   .then((response)=> response.json())
+   .then((data) => {
+    setArtSection(data.data)
+    console.log(data.data)
+  })
+}, [])
+
   return (
     <div className="main-layout">
-      <ArtsSection />
-      <UsersSection />
+      <ArtsSection  artSection={artSection}/>
+      <UsersSection  userSection={userSection}/>
       <AdviceSection />
     </div>
   )
--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,34 @@
-function ArtsSection() {
+function ArtsSection({artSection}) {
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+        {artSection.map((art) => {
+          if (art.subject_titles && art.subject_titles.length > 0) {
+            return (
+              <li key={art.id}>
+                <div className="frame">
+                  <img src="https://i.imgur.com/n6Dc0QL.jpeg" alt="Mountain Lake" />
+                  </div>
+                <h3>Hunted House</h3>
+                <p>{`Artist: ${art.title}`}</p>
+                <h4>Artistic Subjects</h4>
+                <ul>
+                  {art.subject_titles.map((subjectTitle) => {
+                    return (
+                      <li>{subjectTitle}</li>
+                    )
+                  })}
+                </ul>
+              </li>
+            )
+           }
+          })}
+
+
+        </ul>
+      </div>      
     </section>
   )
 }
