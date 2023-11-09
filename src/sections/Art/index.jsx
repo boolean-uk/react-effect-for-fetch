cc9b6d32a15a688195df798f15ab43b679d6aa90
Hamza Ahmed <hamza.anwar@outlook.com>
2023-11-09 23:06:47 +0000

    done core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,29 @@
+import ArtList from "./components/ArtList";
+import { useState, useEffect } from "react";
 function ArtsSection() {
+  const [artList, setArtList] = useState([]);
+
+  const GETData = () => {
+    fetch(
+      `https://api.artic.edu/api/v1/artworks
+      `
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setArtList(data.data);
+        // console.log(artList);
+      });
+  };
+  useEffect(GETData, []);
+  // console.log(artList);
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className='scroll-container'>
+        <ArtList artList={artList} />
+      </div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
