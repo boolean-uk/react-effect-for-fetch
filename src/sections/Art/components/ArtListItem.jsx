cd06162b206cdfdb5f0736ceae47015c0d247737
Radio58 <rt58gm@gmail.com>
2024-02-07 13:05:54 +0000

    Formatting changes

--- src/sections/Art/components/ArtListItem.jsx
+++ src/sections/Art/components/ArtListItem.jsx
@@ -1,17 +1,17 @@
 import PublicationHistoryList from "./PublicationHistoryList";
 
 function ArtListItem({ title, artist, imageURL, publicationHistory }) {
-    return (
-      <>
-        <li>
-            <h3>{title}</h3>
-            <p>{artist}</p>
-            <img src={`https://boolean-api-server.fly.dev${imageURL}`}/>
-            <PublicationHistoryList publicationHistory={publicationHistory}/>
-        </li>
-      </>
-    );
-  };
-  
-  export default ArtListItem;
+  return (
+    <>
+      <li>
+          <h3>{title}</h3>
+          <p>{artist}</p>
+          <img src={`https://boolean-api-server.fly.dev${imageURL}`}/>
+          <PublicationHistoryList publicationHistory={publicationHistory}/>
+      </li>
+    </>
+  );
+}
+
+export default ArtListItem;
   
