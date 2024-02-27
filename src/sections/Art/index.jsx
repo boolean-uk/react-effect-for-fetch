5af9df5f84963974ca76bfc9ba566a227c8e54e0
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-27 11:20:44 +0100

    Finished core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,50 @@
-function ArtsSection() {
+import { useState } from "react"
+import { useEffect } from "react"
+
+const URL = "https://boolean-api-server.fly.dev"
+
+function ArtsSection() {  
+  const [data, setData] = useState([])
+
+  useEffect(() => {console.log("Fetch art from...", `${URL}/art`)
+    fetch(`${URL}/art`, {}).then(
+      (response) => {
+        return response.json();
+      }).then((jsonData) =>{
+        console.log(jsonData)
+        setData(jsonData)
+      })
+      }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className = "art-list">
+        {data.map((item, index) => {
+          return (
+            <li key = {index}>
+            <div className="frame">
+              <img src={`${URL}${item.imageURL}`}/>
+            </div>
+            <h3>{item.title}</h3>
+            <p>Artist: {item.artist}</p>
+            <h4>Publication History:</h4>
+            <ul>
+              {    
+                item.publicationHistory.map((history, index) => {
+                  return (
+                    <li key = {index}>{history}</li>
+                  )
+                })              
+              }
+            </ul>
+          </li>
+          )
+
+        })}
+        </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+const URL ="https://boolean-api-server.fly.dev/Annemoon-de-Groen/contact"
+
 function UsersSection() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {console.log("Fetch users from...", URL)
+  fetch(URL, {}).then(
+    (response) => {
+      return response.json();
+    }).then((jsonData) =>{
+      console.log(jsonData)
+      setUsers(jsonData)
+    })
+    }, [])
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {
+            users.map((user, index) =>{
+              return(
+                <li style={{background: user.favouriteColour}}>
+                  <img src = {user.profileImage} alt = {user.firstName}/>
+                  <h3>{user.firstName} {user.lastName}</h3>
+                  <p>Email: {user.email}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </section>
   )
 }
