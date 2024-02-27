6851ac0a6c402440d1ed60e445a3739ac1a11ac2
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-02-27 13:44:50 +0100

    core and extension

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import "./App.css";
 function App() {
   const [artData, setArtData] = useState(null);
   const [userData, setUserData] = useState(null);
+  const [advice, setAdvice] = useState(null);
 
-  console.log({ artData });
-  console.log({ userData });
+  console.log({ advice });
 
   useEffect(() => {
     console.log(
@@ -19,24 +19,37 @@ function App() {
     fetch(`https://boolean-api-server.fly.dev/art`)
       .then((response) => response.json())
       .then((data) => {
-        console.log("received data:", data);
-        console.log("setting new state....");
         setArtData(data);
       });
     fetch(`https://boolean-api-server.fly.dev/JacobChenJensen/contact`)
       .then((response) => response.json())
       .then((data) => {
-        console.log("received data:", data);
-        console.log("setting new state....");
         setUserData(data);
       });
+    fetch(`https://api.adviceslip.com/advice`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("received data:", data);
+        console.log("setting new state....");
+        setAdvice(data);
+      });
   }, []);
+
+  const fetchNewAdvice = () => {
+    fetch(`https://api.adviceslip.com/advice`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("received data:", data);
+        console.log("setting new state....");
+        setAdvice(data);
+      });
+  };
 
   return (
     <div className="main-layout">
       <ArtsSection artData={artData} />
       <UsersSection userData={userData} />
-      <AdviceSection />
+      <AdviceSection advice={advice} fetchNewAdvice={fetchNewAdvice} />
     </div>
   );
 }
--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,63 @@
-function AdviceSection() {
+import { useState } from "react";
+function AdviceSection({ advice, fetchNewAdvice }) {
+  const [favouriteAdvice, setFavouriteAdvice] = useState([]);
+
+  const handleSaveToFavourites = () => {
+    if (
+      advice !== null &&
+      !favouriteAdvice.some(
+        (existingAdvice) => existingAdvice.slip.id === advice.slip.id
+      )
+    ) {
+      setFavouriteAdvice((prevFavourites) => [...prevFavourites, advice]);
+    }
+  };
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="advice-slip">
+        {advice !== null ? (
+          <>
+            <p>{advice.slip.advice}</p>
+            <button onClick={fetchNewAdvice}>Get More Advice</button>
+            <button onClick={handleSaveToFavourites}>Save to Favourties</button>
+          </>
+        ) : (
+          <li>No advice available</li>
+        )}
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        {favouriteAdvice !== null ? (
+          <ul>
+            {favouriteAdvice.map((advice, index) => (
+              <li key={index}>{advice.slip.advice}</li>
+            ))}
+          </ul>
+        ) : (
+          <li>No favourite advice yet.</li>
+        )}
+      </section>
     </section>
-  )
+  );
 }
+/*<section>
+      <h2>Advice Section</h2>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>Always the burrito.</p>
+        <button>Get More Advice</button>
+        <button>Save to Favourties</button>
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          <li>Measure twice, cut once.</li>
+          <li>Don't let the bastards grind you down.</li>
+          <li>Always the burrito.</li>
+        </ul>
+      </section>
+    </section>*/
 
-export default AdviceSection
+export default AdviceSection;
