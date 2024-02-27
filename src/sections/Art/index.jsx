9bdb827870c54b2457d1c79d9f50ae5ab5a5331f
Najem Hamo <najem.hamo@gmail.com>
2024-02-27 12:11:58 +0100

    Core - without components

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,53 @@
+import "../../App.css";
+import { useState, useEffect } from "react";
+
 function ArtsSection() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://boolean-api-server.fly.dev/art")
+      .then((response) => response.json())
+      .then((data) => {
+        setArtworks(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <section>
-      <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
+    <>
+      <section>
+        <h2>Arts Section</h2>
+        <div className="scroll-container">
+          <ul className="art-list">
+            {artworks.map((artwork) => {
+              return (
+                <li key={artwork.id}>
+                  <img
+                    src={
+                      `https://boolean-api-server.fly.dev/` + artwork.imageURL
+                    }
+                    alt={artwork.title}
+                  />
+                  <br />
+                  The artist&apos;s name: {artwork.artist} <br />
+                  The title of the artwork: {artwork.title}
+                  <br />
+                  The complete publication history: <br />
+                  <ul>
+                    {artwork.publicationHistory.map((publicationHistory) => {
+                      return (
+                        <li key={publicationHistory}>{publicationHistory}</li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,43 @@
+import "../../App.css";
+import { useState, useEffect } from "react";
+
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://boolean-api-server.fly.dev/najemhamo/contact")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
-    <section>
-      <h2>Users Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
+    <>
+      <section>
+        <h2>Users Section</h2>
+        <div className="scroll-container">
+          <ul className="users-list">
+            {users.map((user) => {
+              return (
+                <li style={{ background: user.favouriteColour }} key={user.id}>
+                  <img
+                    src={`https://www.gravatar.com/avatar/${user.email}?s=120&d=identicon`}
+                    alt={user.name}
+                  />
+                  <h3>
+                    {user.firstName} {user.lastName}
+                  </h3>
+                  <h3>Email: {user.email}</h3>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default UsersSection
+export default UsersSection;
