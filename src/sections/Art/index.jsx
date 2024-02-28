0f8dcae74566724702766f50e917640e0491364f
Sebank <sebank@gmail.com>
2024-02-28 15:31:22 +0100

    firefox is being weird

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,48 @@
+import { useState, useEffect } from "react";
+
 function ArtsSection() {
+
+  const [art, setArt] = useState([{publicationHistory: ["a"]}])
+
+  const url = "https://boolean-api-server.fly.dev/art"
+
+  useEffect(() => {
+    
+    const fetchdata = async () => {
+      const response = await fetch(url);
+      const newData = await response.json();
+      setArt(newData);
+    };
+
+    fetchdata();
+
+  }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+         <ul className="art-list">
+          {art.map((elm, key) => 
+            <li key={key}>
+            <div className="frame">
+              <img
+                src={"https://boolean-api-server.fly.dev" + elm.imageURL}
+              />
+            </div>
+            <h3>{elm.title}</h3>
+            <p>Artist: {elm.artist}</p>
+            <h4>Publication History:</h4>
+            <ul>
+              {elm.publicationHistory.map((pub) => 
+              <li>
+                {pub}
+              </li>)}
+            </ul>
+          </li>
+          )}
+        </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,46 @@
+import { useState, useEffect } from "react";
+
 function UsersSection() {
+
+  const [users, setUsers] = useState([
+    {email: "email", firstName: "firstName", 
+    lastName: "lastName", favouriteColour: "#5894ff",
+    profileImage:	"https://www.gravatar.com/avatar/Selina_Wuckert@hotmail.com?s=120&d=identicon"}
+  ]);
+
+  const url = "https://boolean-api-server.fly.dev/Sebank/contact";
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const newData = await response.json();
+      setUsers(newData);
+      console.log(newData)
+    }
+    fetchData()
+  }, [])
+
+  console.log("users", users)
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {users.map((elm, key) => 
+            <li key={key} style={{background: elm.favouriteColour}}>
+            <img
+              src={elm.profileImage}
+              alt="Ritthy Ryan"
+            />
+            <h3>{elm.gender}: {elm.firstName} {elm.lastName}</h3>
+            <p>Email: {elm.email}</p>
+          </li>
+          )}
+        </ul>
+
+      </div>
     </section>
   )
 }
