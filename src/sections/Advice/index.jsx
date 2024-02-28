35fa1dfbc4f5ea7ce21df2adab258c6ae2be3963
spectraldesign <spectraldesign1@gmail.com>
2024-02-28 11:04:23 +0100

    Extension

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
+import AdviceSlip from './components/AdviceSlip';
+import FavouriteSlipsList from './components/FavouriteSlipsList';
+
+const apiUrl = 'https://api.adviceslip.com/advice';
 function AdviceSection() {
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [adviceSlip, setAdviceSlip] = useState(null);
+  const [favouriteSlips, setFavouriteSlips] = useState([]);
+
+  useEffect(() => {
+    console.log('Fetching advice')
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAdviceSlip(data);
+      });
+  }, [shouldFetch]);
+
+  const addToFavourites = () => {
+    if (!adviceSlip) return;
+    if (favouriteSlips.includes(adviceSlip.slip.advice)) return;
+    setFavouriteSlips([...favouriteSlips, adviceSlip.slip.advice]);
+  }
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <AdviceSlip adviceSlip={adviceSlip} setShouldFetch={setShouldFetch} shouldFetch={shouldFetch} addToFavourites={addToFavourites} />
+      <FavouriteSlipsList favouriteSlips={favouriteSlips} />
     </section>
   )
 }
