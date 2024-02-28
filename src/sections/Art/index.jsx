65d3100f8b27de756c643b8eaa31e2647efb2fb7
spectraldesign <spectraldesign1@gmail.com>
2024-02-28 10:38:43 +0100

    mvp

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from 'react';
+const baseAPIUrl = 'https://boolean-api-server.fly.dev';
 function ArtsSection() {
+  const [artList, setArtList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseAPIUrl}/art`)
+      .then((response) => response.json())
+      .then((data) => setArtList(data));
+  }, []);
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          {
+            artList.map((art, index) => (
+              <li key={index}>
+                <div className='frame'>
+                  <img src={`${baseAPIUrl}/${art.imageURL}`} />
+                </div>
+                <h3>{art.title}</h3>
+                <p>Artist: {art.artist}</p>
+                <h4>Publication History:</h4>
+                <ul>
+                  {
+                    art.publicationHistory.map((history, index) => (
+                      <li key={index}>{history}</li>
+                    ))
+                  }
+                </ul>
+              </li>
+            ))
+          }
+        </ul>
+
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+
+const baseAPIUrl = 'https://boolean-api-server.fly.dev';
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseAPIUrl}/spectraldesign/contact`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {
+            users.map((user, index) => (
+              <li key={index} style={{ background: user.favouriteColour }}>
+                <img src={`https://www.gravatar.com/avatar/${user.email}?s=120&d=identicon`} alt={user.name} />
+                <h3>{`${user.firstName} ${user.lastName}`}</h3>
+                <p>Email: {user.email}</p>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+
     </section>
   )
 }
