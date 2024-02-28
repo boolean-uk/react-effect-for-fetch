9397cd7faf86d075171d2a066cccad1dbf471768
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-02-28 13:49:32 +0100

    Advice Section Done

---
+++ src/sections/Users/components/UsersListItem.jsx
@@ -0,0 +1,10 @@
+export default function UsersListItem({ user }) {
+    const fullname = `${user.firstName} ${user.lastName}`;
+    return (
+      <li>
+        <img className= "icon" src={user.profileImage} alt={fullname} />
+        <h3>{fullname}</h3>
+        <p>Email: {user.email}</p>
+      </li>
+    );
+  }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import UsersList from "./components/UsersList";
+
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch(
+        "https://boolean-api-server.fly.dev/malimo326/contact"
+      );
+      const json = await response.json();
+      setUsers(json);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <UsersList users={users} />
+      </div>
     </section>
   )
 }
