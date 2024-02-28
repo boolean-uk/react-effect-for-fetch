564db4b5b21904c3f09fe787ad1dc4452115073e
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-02-28 13:31:36 +0100

    Completed Core

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import { UsersList } from "./components/UsersList"
+
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://boolean-api-server.fly.dev/olemarkusroland/contact");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <UsersList users={users} />
+      </div>
     </section>
   )
 }
