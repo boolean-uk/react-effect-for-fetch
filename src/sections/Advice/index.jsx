354ba07844fc46cdfa61b1a96ca81a347a931c21
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-02-29 15:28:54 +0100

    core + extension

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,58 @@
+import { useState, useEffect } from "react";
+
 function AdviceSection() {
-  return (
-    <section>
-      <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
-    </section>
-  )
+
+  const [advice, setAdvice] = useState(null)
+  const [favAdvices, setFavAdvices] = useState([])
+  const address = 'https://api.adviceslip.com/advice'
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    await fetch(address)
+      .then(res => res.json())
+      .then(data => setAdvice(data))
+  }
+
+  const saveToFavorites = () => {
+    if (advice && !favAdvices.includes(advice))
+    {
+      setFavAdvices(prev => [...prev, advice])
+    }
+  }
+
+  if (advice)
+  {
+    return (
+      <section>
+        <h2>Advice Section</h2>
+        <section className="adivce-slip">
+          <h3>Some Advice</h3>
+          <p>{advice.slip.advice}</p>
+          <button
+            onClick={fetchData}
+          >Get More Advice</button>
+          <button
+            onClick={saveToFavorites}
+          >Save to Favourties
+          </button>
+        </section>
+        <section className="favourtite-slips-list">
+          <h3>Favourite Advice Slips</h3>
+          <ul>
+            {
+              favAdvices ? favAdvices.map((a, i) => {
+                return (<li key={i}>{a.slip.advice}</li>)
+              }) : <p>No advices favorited</p>
+            }
+          </ul>
+        </section>
+      </section>
+    )
+
+  }
 }
 
 export default AdviceSection
