802a94b06174531bef01bb76b67ac46ba766c9ad
Matthias Greeven <matsgreeven@gmail.com>
2024-02-29 10:46:58 +0100

    advice finished

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,9 +1,31 @@
+import AdviceSlip from "./components/AdviceSlip"
+import FavouriteSlipsList from "./components/FavouriteSlipsList"
+import { useEffect, useState } from 'react'
+
 function AdviceSection() {
+  const [favorites, setFavorites] = useState([])
+  const [slip, setSlip] = useState(undefined)
+
+  async function fetchSlip() {
+      await fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(setSlip)
+    }
+    
+    function addToFavorites() {
+      if (!favorites.includes(slip))
+      setFavorites([...favorites, slip])
+    }
+
+    useEffect(() => {
+      fetchSlip()
+    }, [])
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <AdviceSlip slip={slip} fetchSlip={fetchSlip} addToFavorites={addToFavorites} />
+      <FavouriteSlipsList favorites={favorites}/>
     </section>
   )
 }
