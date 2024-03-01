c2b9ec532238cbc235d840b5ea7ec8fcd1510eba
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-03-01 12:50:04 +0100

    Art section

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from 'react'
+
 function ArtsSection() {
+  const [artworks, setArtworks] = useState([])
+
+  useEffect(() => {
+    fetch("https://boolean-api-server.fly.dev/art")
+      .then(response => response.json())
+      .then(setArtworks)
+  }, [])
+
+  console.log(artworks)
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          {artworks.length !== 0 && 
+          artworks.map((art, index) => 
+            (<li key={index}>
+              <div className="frame">
+                <img src={`https://boolean-api-server.fly.dev/${art.imageURL}`} />
+              </div>
+              <h3>{art.title}</h3>
+              <p>Artist: {art.artist}</p>
+              <h4>Publication history:</h4>
+              {art.publicationHistory.map((publication, index) => (
+                <li key={index}>
+                  {publication}
+                </li>
+              ))}
+            </li>)
+          )}
+        </ul>
+      </div>
+
     </section>
   )
 }
