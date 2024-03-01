812cfa2ca6ecfa817b2071f3df9821440132d6f3
pkekkonen <paulina.kekkonen@hotmail.com>
2024-03-01 13:21:32 +0100

    Core completed

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
+import UsersList from "./components/UsersList";
+
+const GITHUB_USERNAME = "pkekkonen";
+
 function UsersSection() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://boolean-api-server.fly.dev/${GITHUB_USERNAME}/contact`)
+      .then((response) => {
+        const data = response.json();
+        return data;
+      })
+      .then((responseData) => {
+        setUsers(responseData);
+      });
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {users && <UsersList users={users} />}
+      </div>
     </section>
-  )
+  );
 }
 
-export default UsersSection
+export default UsersSection;
