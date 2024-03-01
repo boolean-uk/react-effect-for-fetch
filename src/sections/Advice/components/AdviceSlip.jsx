282470d497001d8c01c5559db3063b8ed338ef82
Gustav Svennas <gustavsvennas@gmail.com>
2024-03-01 15:54:57 +0100

    Done with extension

--- src/sections/Advice/components/AdviceSlip.jsx
+++ src/sections/Advice/components/AdviceSlip.jsx
@@ -1,8 +1,18 @@
 function AdviceSlip(props) {
+    const { slip, newAdvice, addFavAdvice } = props;
 
-    const [advice, setAdvice] = user
+    const addSlipToFav = () => {
+        addFavAdvice(slip)
+    }
 
     return (
-
-    )
+      <section className="advice-slip">
+        <h3>Some Advice</h3>
+        <p>{slip.advice}</p>
+        <button onClick={newAdvice}>Get More Advice</button>
+        <button onClick={addSlipToFav}>Save to Favourites</button>
+      </section>
+    );
 }
+
+export default AdviceSlip
--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,45 @@
+import { useState, useEffect } from "react";
+import AdviceSlip from "./components/AdviceSlip";
+import FavouriteSlipsList from "./components/FavouriteSlipsList";
+
 function AdviceSection() {
+
+  const [adviceData, setAdviceData] = useState(null);
+
+  const [favAdvices, setFavAdvices] = useState([])
+
+  const newAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => setAdviceData(data))
+  };
+
+  useEffect(() => {
+    newAdvice()
+  }, []);
+
+  const addFavAdvice = (newSlip) => {
+    for (const elem of favAdvices) {
+      if (elem.id === newSlip.id) {
+        return
+      }
+    }
+    setFavAdvices([...favAdvices, newSlip]);
+  }
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="advice-slip"></section>
-      <section className="favourite-slips-list"></section>
+      {adviceData && (
+        <AdviceSlip
+          slip={adviceData.slip}
+          newAdvice={newAdvice}
+          addFavAdvice={addFavAdvice}
+        />
+      )}
+      <FavouriteSlipsList favAdvices={favAdvices} />
     </section>
-  )
+  );
 }
 
 export default AdviceSection
