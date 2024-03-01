3a2c19769ced8599f04715c23288153371f87671
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-01 15:32:52 +0100

    core done

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -11,16 +11,15 @@ export default function ArtsSection() {
 
   return (
     <section>
-      <h2>Arts Section</h2>
+      <code><h2>Arts Section</h2></code>
+      <div className="scroll-container">
       <ul className="art-list">
         {artworks.map((artwork) => (
           <li key={artwork.id}>
             <div className="frame">
-              <img
-                src={`https://boolean-api-server.fly.dev${artwork.imageURL}`}
-                alt={artwork.title}
-              />
+              <img src={`https://boolean-api-server.fly.dev${artwork.imageURL}`}/>
             </div>
+            <code>
             <h3>{artwork.title}</h3>
             <p>Artist: {artwork.artist}</p>
             <h4>Publication History:</h4>
@@ -29,10 +28,11 @@ export default function ArtsSection() {
                 <li key={index}>{publication}</li>
               ))}
             </ul>
+        </code>
           </li>
         ))}
       </ul>
-      <div className="scroll-container"></div>
+    </div>
     </section>
   );
 }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
+
 function UsersSection() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://boolean-api-server.fly.dev/nicolaiklokmose/contact`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
     <section>
-      <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <code><h2>Users Section</h2></code>
+      <div className="scroll-container">
+        <ul class="users-list">
+          {users.map((user, index) => (
+            <li key={index} style={{ backgroundColor: user.favouriteColour }}>
+              <img src={user.profileImage}/>
+              <code>
+              <h3>{user.firstName + " " + user.lastName} <br/> </h3>
+              <strong>Gender:</strong> {user.gender} <br/>
+              <strong>Email:</strong> {user.email} <br/>
+              <strong>Job Title:</strong> {user.jobTitle} <br/>
+              <p>{user.city}, {user.street}</p>
+              </code>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
 
-export default UsersSection
+export default UsersSection;
