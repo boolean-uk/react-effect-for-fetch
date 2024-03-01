a22dcc3a9342db800e136bf4ee5588c5dc048ee7
noahlenn <noahlenn@hotmail.com>
2024-03-01 14:55:46 +0100

    core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,44 @@
+import { useState, useEffect } from 'react';
+
+
 function ArtsSection() {
-  return (
-    <section>
-      <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
+  const [art, setArt] = useState([]);
+
+  console.log(art)
+  
+  useEffect(() => {
+    fetch('https://boolean-api-server.fly.dev/art')
+      .then(response => response.json())
+      .then(responseData => setArt(responseData))
+  }, []); 
+
+return (
+  <section>
+    <h2>Arts Section</h2>
+    <div className="scroll-container">
+      <ul style={{ listStyleType: 'none' }}>
+        {art.map((artElement, id) => (
+          <li key={id}>
+            <img src={`https://boolean-api-server.fly.dev${artElement.imageURL}`} width={350} />
+            <div>
+              <p>{artElement.title}</p>
+              <p>Artist: {artElement.artist}</p>
+
+              <ul>
+                {artElement.publicationHistory.map((publication, id) => (
+                  <li key={id}>    
+                    {publication}              
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </section>
+)
+
 }
 
 export default ArtsSection
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from 'react';
+
+
 function UsersSection() {
+
+  const [users, setUsers] = useState([]);
+
+  console.log(users)
+  
+  useEffect(() => {
+    fetch('https://boolean-api-server.fly.dev/noahlenn/contact')
+      .then(response => response.json())
+      .then(responseData => setUsers(responseData))
+  }, []); 
+
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+
+        <ul className="users-list">
+          {users.map((user, id) => (
+            // {user.favouriteColour}
+            <li key={id} style={{ backgroundColor: user.favouriteColour }}>
+
+              <img src={user.profileImage} width={100} />
+              <div>
+                <p>{user.firstName} {user.lastName}</p>
+                <p>Email: {user.email}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </section>
   )
 }
