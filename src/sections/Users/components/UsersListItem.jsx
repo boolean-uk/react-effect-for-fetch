d410a4406c22eb65fc6fc255f989de5d757960ba
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-03-01 10:18:46 +0100

    React: Made Art-List and User-List Pages and Fetched Data Through useEffect Hook

---
+++ src/sections/Users/components/UsersListItem.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+function UsersListItem({ userData }) {
+  return (
+    <div style={{ background: userData.favouriteColour }}>
+      <img src={userData.profileImage} alt="Ritthy Ryan" />
+      <h3>{userData.firstName + " " + userData.lastName}</h3>
+      <p>{userData.email}</p>
+    </div>
+  );
+}
+
+UsersListItem.propTypes = {
+  userData: PropTypes.object.isRequired,
+};
+
+export default UsersListItem;
