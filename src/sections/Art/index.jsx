fadab6a6b6f6cb67790f06822fcd343e360580e0
Maha Riazi <maha.riazi@hotmail.no>
2024-03-01 12:44:42 +0100

    art complete, components missing

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react"
+
+const baseURL = "https://boolean-api-server.fly.dev"
+
 function ArtsSection() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch(baseURL + "/art")
+      .then((response) => response.json())
+      .then(setData)
+  }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+
+      <div className="scroll-container">
+        <ul className="art-list">
+          {data.map((item, index) => (
+            <li key={index}>
+              <div className="frame">
+                <img src={baseURL+item.imageURL} />
+              </div>
+
+              <h3>{item.title}</h3>
+              <p>Artist: {item.artist}</p>
+              <h4>Publication History:</h4>
+
+              <ul>
+                {item.publicationHistory.map((history, i) =>(
+                  <li key={i}>{history}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
