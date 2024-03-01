28ed7cd97d6d4fba23d210a611da6cb37297ca23
tonnesegeland <to.smith.eg@gmail.com>
2024-03-01 17:11:03 +0100

    Extension 1 done. Handles work, but fetch fetches same advise severeal times in a row sometimes

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
+
+const initAdvice = "Opinions are like arseholes, everyone has one.";
+
 function AdviceSection() {
+  const [data, setData] = useState();
+  const [advice, setAdvice] = useState(initAdvice);
+  const [favourite, setFavourite] = useState([]);
+  const [update, setUpdate] = useState(0);
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then(setData);
+  }, [update]);
+
+  const handleGetMoreAdvices = (e) => {
+    e.preventDefault();
+
+    setUpdate(update + 10);
+    setAdvice(data.slip.advice);
+  };
+
+  const handleFavourite = (e) => {
+    e.preventDefault();
+    setFavourite([...favourite, advice]);
+
+    setUpdate(update + 10)
+    setAdvice(data.slip.advice)
+  };
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>{advice}</p>
+        <button onClick={handleGetMoreAdvices}>Get More Advice</button>
+        <button onClick={handleFavourite}>Save to Favourties</button>
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          {favourite.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </section>
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
