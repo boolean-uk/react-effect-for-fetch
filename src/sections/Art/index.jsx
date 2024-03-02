acd34c50dd3d077236153b7c0dbb5aa688c38f7c
Axel Hansson <axel.co.hansson@gmail.com>
2024-03-02 16:08:16 +0100

    core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,30 @@
-function ArtsSection() {
+import { useEffect, useState } from "react"
+import ArtList from "./components/ArtList"
+import PropTypes from 'prop-types'
+
+function ArtsSection({baseUrl}) {
+  const[artObjects, setArtObjects] = useState([])
+  const [artUrl, SetArtUrl] = useState(`${baseUrl}art`) 
+
+  useEffect(() => {
+    fetch(artUrl)
+      .then((res) => res.json())
+      .then((data) =>{setArtObjects(data)})
+  }, [artUrl])
+
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ArtList artObjects={artObjects} baseUrl={baseUrl}></ArtList>
+      </div>
     </section>
   )
+}
+
+ArtsSection.propTypes = {
+  baseUrl: PropTypes.string
 }
 
 export default ArtsSection
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,30 @@
-function UsersSection() {
+import { useEffect, useState } from "react"
+import PropTypes from 'prop-types'
+import UsersList from "./components/UsersList"
+
+function UsersSection({baseUrl}) {
+  const [users, SetUsers] = useState([]) 
+  const [userUrl, SetUserUrl] = useState(`${baseUrl}AxelHan/contact`) 
+
+  useEffect(() => {
+    fetch(userUrl)
+      .then((res) => res.json())
+      .then((data) =>{SetUsers(data)})
+  }, [userUrl])
+
+  console.log(users)
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <UsersList users={users}></UsersList>
+      </div>
     </section>
   )
+}
+
+UsersSection.propTypes = {
+  baseUrl: PropTypes.string
 }
 
 export default UsersSection
