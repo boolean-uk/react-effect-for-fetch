ba4d72d687049796dbdcac8ed6d6ea2bfc026b1e
Mathias Fredriksen <mathias.fredriksen@no.experis.com>
2024-03-04 12:42:54 +0100

    Core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
+import ArtListComponent from "./components/ArtList"
+
 function ArtsSection() {
+  const [artListData, setArtListData] = useState([]);
+  const baseURL = "https://boolean-api-server.fly.dev"
+
+  useEffect(() => {
+    fetch(baseURL + "/art")
+    .then((res) => res.json())
+    .then(setArtListData)
+  }, [])
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ArtListComponent artListData={artListData} baseURL={baseURL} />
+      </div>
     </section>
   )
 }
