1b5b143061e5be4fd99fb77e3831ddbe07158cdd
Periklis <perik76@gmail.com>
2024-05-02 00:20:33 +0300

    Core done

---
+++ src/sections/Advice/components/AdviceSlip.jsx
@@ -0,0 +1,12 @@
+//https://api.adviceslip.com/advice
+
+export default function AdviceSlip() {
+	return (
+		<section className='adivce-slip'>
+			<h3>Some Advice</h3>
+			<p>Always the burrito.</p>
+			<button>Get More Advice</button>
+			<button>Save to Favourties</button>
+		</section>
+	)
+}
