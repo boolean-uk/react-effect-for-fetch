7d0e130df3e2bb28ef36806b0dcb00f24c7ef5dc
Leonardo Lodi <your_email@address>
2024-05-01 14:57:15 +0100

    do art section

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 function ArtsSection() {
+  const [arts, setArts] = useState([])
+  
+  useEffect(() => {
+    fetch('https://boolean-uk-api-server.fly.dev/art')
+      .then(res => res.json())
+      .then(setArts)
+  }, [])
+  
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {arts.map(art => 
+          <ul key={art.id} className="art-list">
+            <li>
+              <div className="frame">
+                <img
+                  src={`https://boolean-api-server.fly.dev${art.imageURL}`}
+                />
+              </div>
+              <h3>{art.title}</h3>
+              <p>Artist: {art.artist}</p>
+              <h4>Publication History:</h4>
+              <ul>
+                <li>{art.publicationHistory[0]}</li>
+                <li>{art.publicationHistory[1]}</li>
+                <li>{art.publicationHistory[2]}</li>
+              </ul>
+            </li>
+          </ul>
+        )}
+      </div>
     </section>
   )
 }
