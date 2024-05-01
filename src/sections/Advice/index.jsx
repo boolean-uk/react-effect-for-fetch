cf44935ce0ee70ee0ee6ea05dfeba26d3a4424de
Tim Zoltie <t.zoltie@gmail.com>
2024-05-01 13:04:15 +0100

    Add: art section completed, still requires to be broken down into components

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+
 function AdviceSection() {
+
+  const [advice, setAdvice] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/')
+      .then(response => response.json())
+      .then(setAdvice)
+  }, [advice])
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>Always the burrito.</p>
+        <button>Get More Advice</button>
+        <button>Save to Favourites</button>
+      </section>
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          <li>Measure twice, cut once.</li>
+          <li>Don't let the basterds grind you down.</li>
+          <li>Always the burrito.</li>
+        </ul>
+      </section>
     </section>
   )
 }
--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react"
+
 function ArtsSection() {
+
+  const[art, setArt] = useState([])
+
+  useEffect(() => {
+    fetch('https://boolean-uk-api-server.fly.dev/art')
+      .then(response => response.json())
+      .then(setArt)
+      return;
+  }, [])
+
+  console.log(art)
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          {art.map((painting, index) => 
+          <li key={index}>
+            <div className="frame">
+              <img src={`https://boolean-api-server.fly.dev${painting.imageURL}`}/>
+            </div>
+            <h3>{painting.title}</h3>
+            <p>{painting.artist}</p>
+            <h4>Publication History:</h4>
+            <ul>
+              {painting.publicationHistory.map((info, index) =>
+                <li key={index}>{info}</li>)}
+            </ul>
+          </li>)}
+        </ul>
+      </div>
     </section>
   )
 }
