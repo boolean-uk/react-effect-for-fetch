9ae138032c46eab915e1ce9a67a5c51346b8f5e7
Leonardo Lodi <your_email@address>
2024-05-01 14:58:04 +0100

    do user section

--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 function UsersSection() {
+  const [users, setUsers] = useState([])
+  
+  useEffect(() => {
+    fetch('https://boolean-uk-api-server.fly.dev/LeonardoSaraceli/contact')
+      .then(res => res.json())
+      .then(setUsers)
+  }, [])
+  
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {users.map(user => 
+          <ul key={user.id} className="users-list">
+            <li style={{ background: `${user.favouriteColour}`}}>
+              <img
+                src={`https://www.gravatar.com/avatar/${user.email}?s=120&d=identicon`}
+                alt={`${user.firstName} ${user.lastName}`}
+              />
+              <h3>{user.gender.toLowerCase().includes('female') ? 'Ms' : 'Mr'} {user.firstName} {user.lastName}</h3>
+              <p>Email: {user.email}</p>
+            </li>
+          </ul>
+          )}
+      </div>
     </section>
   )
 }
