df6ecc7a4934f4974e09389ba4e100a4646311a3
Shaun Harris <shaun@shaunwilliamharris.com>
2024-05-24 14:51:15 +0100

    advice slip comps and props, ext req met

--- src/sections/Advice/index.jsx
+++ src/sections/Advice/index.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import AdviceSlip from "./components/AdviceSlip"
+import FavouriteSlipsList from "./components/FavouriteSlipsList"
+
 function AdviceSection() {
+  const [adviceUrl, setAdviceUrl] = useState("https://api.adviceslip.com/advice")
+  const [advice, setAdvice] = useState({})
+  const [adviceList, setAdviceList] =useState ([])
+
+const generateAdvice = () => (
+  fetch(adviceUrl)
+  .then((res) => res.json())
+  .then((data) => {setAdvice(data.slip)})
+) 
+
+useEffect(() => {
+  generateAdvice()
+}, [adviceUrl])
+
+const addToList = () => (
+  setAdviceList((prevList) => [...prevList, advice.advice])
+)
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <AdviceSlip advice={advice} generateAdvice={generateAdvice} addToList={addToList}></AdviceSlip>
+      <section className="favourtite-slips-list">
+      <FavouriteSlipsList adviceList={adviceList}></FavouriteSlipsList>
+      </section>
     </section>
   )
 }
