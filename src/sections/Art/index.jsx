a32cfc227c80d8560d6a314567c6f4045e4a0864
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-06-28 15:12:56 +0100

    Built ArtLi and UserLi in index.js files

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
+
 function ArtsSection() {
+  const [artData, setArtData] = useState([]);
+  console.log("art Data:", artData);
+
+  useEffect(() => {
+    fetch("https://boolean-uk-api-server.fly.dev/art")
+      .then((res) => res.json())
+      .then((data) => setArtData(data));
+  }, []);
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          {artData.map((artItem, index) => (
+            <li key={index}>
+              <div className="frame">
+                <img
+                  src={`https://boolean-api-server.fly.dev${artItem.imageURL}`}
+                />
+              </div>
+              <h3>{artItem.title}</h3>
+              <p>{artItem.artist}</p>
+              <h4>Publication History:</h4>
+              <ul>
+                {artItem.publicationHistory.map((publication, index) => (
+                  <li key={index}>{publication}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+
 function UsersSection() {
+  const [userData, setUserData] = useState([]);
+  console.log("user data", userData);
+
+  useEffect(() => {
+    fetch("https://boolean-uk-api-server.fly.dev/zainabch123/contact")
+      .then((res) => res.json())
+      .then((data) => setUserData(data));
+  }, []);
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="users-list">
+          {userData.map((userItem, index) => (
+            <li
+              key={index}
+              style={{ background: `${userItem.favouriteColour}` }}
+            >
+              <img
+                src={userItem.profileImage}
+                alt={`${userItem.firstName} ${userItem.lastName}`}
+              />
+              <h3>
+                {userItem.firstName} {userItem.lastName}
+              </h3>
+              <p>Email: {userItem.email}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
+  );
 }
 
-export default UsersSection
+export default UsersSection;
